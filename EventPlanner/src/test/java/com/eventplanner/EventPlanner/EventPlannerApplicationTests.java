76086fb522f6658641ca2b38d313066a95eaaba0
oner98 <alioner.filibeli@gmail.com>
2025-12-06 20:30:18 +0300

    Değişiklilikler eklendi

---
+++ EventPlanner/src/test/java/com/eventplanner/EventPlanner/EventPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventplanner.EventPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
